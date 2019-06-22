8d12a467b193ca75a1e632b290bf6b6614d78772
gals <developergals@gmail.com>
2019-06-21 23:35:06 -0300

    commit Primer subida Func OK

---
+++ jscript.js
@@ -0,0 +1,20 @@
+function abrirCiudad (evt, ciudad){
+    var i, tabcontent, tablinks;
+
+    tabcontent=document.getElementsByClassName('tabcontent');
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display='none';        
+    }
+
+    tablinks= document.getElementsByClassName('tablinks');
+    for (let i = 0; i < tablinks.length; i++) {
+        
+        tablinks[i].className=tablinks[i].className.replace(' active','');
+
+    }
+
+    document.getElementById(ciudad).style.display='block';
+    evt.currentTarget.className+=" active";
+
+}
